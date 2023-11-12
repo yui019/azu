4be7263c0efaa33b54f8dd6f0926e6ae2a6fb5c2
yui019 <yui36189@gmail.com>
2023-11-12 23:17:04 +0100

    Slight refactoring again...

--- src/vk_context/init.cpp
+++ src/vk_context/init.cpp
@@ -4,7 +4,6 @@
 #include "VkBootstrap.h"
 #include "../util/vk_init.h"
 #include <SDL_vulkan.h>
-#include <vulkan/vulkan_core.h>
 
 using namespace azu;
 
@@ -19,36 +18,6 @@ VkContext::VkContext(
 	initFramebuffers();
 	initCommands();
 	initSyncStructures();
-}
-
-VkContext::~VkContext() {
-	// if the VkInstance is set to nullptr, it's probably a VkContext left in an
-	// invalid state after being moved, so the destructor shouldn't run
-	if (_instance) {
-		vkDeviceWaitIdle(_device);
-
-		vkDestroyCommandPool(_device, _commandPool, nullptr);
-
-		vkDestroyFence(_device, _renderFence, nullptr);
-		vkDestroySemaphore(_device, _renderSemaphore, nullptr);
-		vkDestroySemaphore(_device, _presentSemaphore, nullptr);
-
-		vkDestroySwapchainKHR(_device, _swapchain, nullptr);
-
-		vkDestroyRenderPass(_device, _renderPass, nullptr);
-
-		for (uint32_t i = 0; i < _framebuffers.size(); i++) {
-			vkDestroyFramebuffer(_device, _framebuffers[i], nullptr);
-
-			vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
-		}
-
-		vkDestroySurfaceKHR(_instance, _surface, nullptr);
-
-		vkDestroyDevice(_device, nullptr);
-		vkb::destroy_debug_utils_messenger(_instance, _debug_messenger);
-		vkDestroyInstance(_instance, nullptr);
-	}
 }
 
 void VkContext::initVulkan(SDL_Window *window, bool useValidationLayers) {
