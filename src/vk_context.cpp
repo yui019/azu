f4a703d53074632c5d31da8599940af5817151d0
yui019 <yui36189@gmail.com>
2023-11-12 02:22:08 +0100

    Do some more error handling and refactor variable names
    Seriously, the guy writing vkguide.dev is wayyyy too messy

--- src/vk_context.cpp
+++ src/vk_context.cpp
@@ -3,6 +3,7 @@
 #include "VkBootstrap.h"
 #include "vk_initializers.h"
 #include <SDL_vulkan.h>
+#include <vulkan/vulkan_core.h>
 
 using namespace azu;
 
@@ -61,51 +62,70 @@ void VkContext::initVulkan(SDL_Window *window, bool useValidationLayers) {
 	vkb::InstanceBuilder builder;
 
 	// make the vulkan instance, with basic debug features
-	auto inst_ret = builder.set_app_name("Azu Application")
-	                    .request_validation_layers(useValidationLayers)
-	                    .use_default_debug_messenger()
-	                    .require_api_version(1, 3, 0)
-	                    .build();
+	auto vkbInstanceResult = builder.set_app_name("Azu Application")
+	                             .request_validation_layers(useValidationLayers)
+	                             .use_default_debug_messenger()
+	                             .require_api_version(1, 3, 0)
+	                             .build();
+	if (!vkbInstanceResult) {
+		throw vkbInstanceResult.error();
+	}
 
-	if (!inst_ret) {}
-
-	vkb::Instance vkb_inst = inst_ret.value();
+	vkb::Instance vkbInstance = vkbInstanceResult.value();
 
 	// grab the instance
-	_instance        = vkb_inst.instance;
-	_debug_messenger = vkb_inst.debug_messenger;
+	_instance        = vkbInstance.instance;
+	_debug_messenger = vkbInstance.debug_messenger;
 
 	SDL_Vulkan_CreateSurface(window, _instance, &_surface);
 
 	// use vkbootstrap to select a gpu.
 	// We want a gpu that can write to the SDL surface and supports vulkan 1.2
-	vkb::PhysicalDeviceSelector selector{vkb_inst};
-	vkb::PhysicalDevice physicalDevice =
-	    selector.set_minimum_version(1, 1).set_surface(_surface).select().value(
-	    );
+	vkb::PhysicalDeviceSelector selector{vkbInstance};
+	auto physicalDeviceResult =
+	    selector.set_minimum_version(1, 1).set_surface(_surface).select();
+	if (!physicalDeviceResult) {
+		throw physicalDeviceResult.error();
+	}
+
+	vkb::PhysicalDevice physicalDevice = physicalDeviceResult.value();
 
 	// create the final vulkan device
 
 	vkb::DeviceBuilder deviceBuilder{physicalDevice};
 
-	vkb::Device vkbDevice = deviceBuilder.build().value();
+	auto vkbDeviceResult = deviceBuilder.build();
+	if (!vkbDeviceResult) {
+		throw vkbDeviceResult.error();
+	}
+
+	vkb::Device vkbDevice = vkbDeviceResult.value();
 
 	// Get the VkDevice handle used in the rest of a vulkan application
 	_device    = vkbDevice.device;
 	_chosenGPU = physicalDevice.physical_device;
 
-	// use vkbootstrap to get a Graphics queue
-	_graphicsQueue = vkbDevice.get_queue(vkb::QueueType::graphics).value();
+	// use vkbootstrap to get a Graphics queue and family
+	auto graphicsQueueResult = vkbDevice.get_queue(vkb::QueueType::graphics);
+	if (!graphicsQueueResult) {
+		throw graphicsQueueResult.error();
+	}
 
-	_graphicsQueueFamily =
-	    vkbDevice.get_queue_index(vkb::QueueType::graphics).value();
+	auto graphicsQueueFamilyResult =
+	    vkbDevice.get_queue_index(vkb::QueueType::graphics);
+	if (!graphicsQueueFamilyResult) {
+		throw graphicsQueueFamilyResult.error();
+	}
+
+	_graphicsQueue       = graphicsQueueResult.value();
+	_graphicsQueueFamily = graphicsQueueFamilyResult.value();
 }
 
 void VkContext::initSwapchain() {
-	vkb::SwapchainBuilder swapchainBuilder{_chosenGPU, _device, _surface};
+	vkb::SwapchainBuilder vkbSwapchainBuilder{_chosenGPU, _device, _surface};
 
 	vkb::Swapchain vkbSwapchain =
-	    swapchainBuilder
+	    vkbSwapchainBuilder
 	        .use_default_format_selection()
 	        // use vsync present mode
 	        .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
@@ -119,6 +139,7 @@ void VkContext::initSwapchain() {
 	_swapchainImageViews = vkbSwapchain.get_image_views().value();
 
 	_swapchainImageFormat = vkbSwapchain.image_format;
+	throw VK_SUCCESS;
 }
 
 void VkContext::initDefaultRenderpass() {
@@ -129,25 +150,25 @@ void VkContext::initDefaultRenderpass() {
 	// so its possible to display it we dont care about stencil, and dont use
 	// multisampling
 
-	VkAttachmentDescription color_attachment = {};
-	color_attachment.format                  = _swapchainImageFormat;
-	color_attachment.samples                 = VK_SAMPLE_COUNT_1_BIT;
-	color_attachment.loadOp                  = VK_ATTACHMENT_LOAD_OP_CLEAR;
-	color_attachment.storeOp                 = VK_ATTACHMENT_STORE_OP_STORE;
-	color_attachment.stencilLoadOp           = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-	color_attachment.stencilStoreOp          = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-	color_attachment.initialLayout           = VK_IMAGE_LAYOUT_UNDEFINED;
-	color_attachment.finalLayout             = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+	VkAttachmentDescription colorAttachment = {};
+	colorAttachment.format                  = _swapchainImageFormat;
+	colorAttachment.samples                 = VK_SAMPLE_COUNT_1_BIT;
+	colorAttachment.loadOp                  = VK_ATTACHMENT_LOAD_OP_CLEAR;
+	colorAttachment.storeOp                 = VK_ATTACHMENT_STORE_OP_STORE;
+	colorAttachment.stencilLoadOp           = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+	colorAttachment.stencilStoreOp          = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+	colorAttachment.initialLayout           = VK_IMAGE_LAYOUT_UNDEFINED;
+	colorAttachment.finalLayout             = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
 
-	VkAttachmentReference color_attachment_ref = {};
-	color_attachment_ref.attachment            = 0;
-	color_attachment_ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+	VkAttachmentReference colorAttachmentRef = {};
+	colorAttachmentRef.attachment            = 0;
+	colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
 
 	// we are going to create 1 subpass, which is the minimum you can do
 	VkSubpassDescription subpass = {};
 	subpass.pipelineBindPoint    = VK_PIPELINE_BIND_POINT_GRAPHICS;
 	subpass.colorAttachmentCount = 1;
-	subpass.pColorAttachments    = &color_attachment_ref;
+	subpass.pColorAttachments    = &colorAttachmentRef;
 
 	// 1 dependency, which is from "outside" into the subpass. And we can read
 	// or write color
@@ -159,35 +180,34 @@ void VkContext::initDefaultRenderpass() {
 	dependency.dstStageMask  = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
 	dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
 
-	VkRenderPassCreateInfo render_pass_info = {};
-	render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
-	render_pass_info.attachmentCount = 1;
-	render_pass_info.pAttachments    = &color_attachment;
-	render_pass_info.subpassCount    = 1;
-	render_pass_info.pSubpasses      = &subpass;
-	render_pass_info.dependencyCount = 1;
-	render_pass_info.pDependencies   = &dependency;
+	VkRenderPassCreateInfo renderPassInfo = {};
+	renderPassInfo.sType           = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+	renderPassInfo.attachmentCount = 1;
+	renderPassInfo.pAttachments    = &colorAttachment;
+	renderPassInfo.subpassCount    = 1;
+	renderPassInfo.pSubpasses      = &subpass;
+	renderPassInfo.dependencyCount = 1;
+	renderPassInfo.pDependencies   = &dependency;
 
-	VK_CHECK(
-	    vkCreateRenderPass(_device, &render_pass_info, nullptr, &_renderPass)
+	VK_CHECK(vkCreateRenderPass(_device, &renderPassInfo, nullptr, &_renderPass)
 	);
 }
 
 void VkContext::initFramebuffers() {
 	// create the framebuffers for the swapchain images. This will connect the
 	// render-pass to the images for rendering
-	VkFramebufferCreateInfo fb_info =
+	VkFramebufferCreateInfo framebufferInfo =
 	    vkinit::framebuffer_create_info(_renderPass, _windowExtent);
 
-	const uint32_t swapchain_imagecount = _swapchainImages.size();
-	_framebuffers = std::vector<VkFramebuffer>(swapchain_imagecount);
+	const uint32_t swapchainImageCount = _swapchainImages.size();
+	_framebuffers = std::vector<VkFramebuffer>(swapchainImageCount);
 
-	for (uint32_t i = 0; i < swapchain_imagecount; i++) {
+	for (uint32_t i = 0; i < swapchainImageCount; i++) {
 
-		fb_info.pAttachments = &_swapchainImageViews[i];
-		VK_CHECK(
-		    vkCreateFramebuffer(_device, &fb_info, nullptr, &_framebuffers[i])
-		);
+		framebufferInfo.pAttachments = &_swapchainImageViews[i];
+		VK_CHECK(vkCreateFramebuffer(
+		    _device, &framebufferInfo, nullptr, &_framebuffers[i]
+		));
 	}
 }
 
