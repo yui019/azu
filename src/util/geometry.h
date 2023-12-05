a8c834d9cdaf8d1de871da71fca36f16d63444ef
yui019 <yui36189@gmail.com>
2023-12-05 23:18:07 +0100

    Add some helper constructors to Vec structs

--- src/util/geometry.h
+++ src/util/geometry.h
@@ -6,12 +6,36 @@ namespace azu {
 struct Vec2 {
 	float x;
 	float y;
+
+	static Vec2 zero() {
+		return {0.0, 0.0};
+	}
+
+	static Vec2 create(float x, float y) {
+		return {x, y};
+	}
+
+	static Vec2 create(int x, int y) {
+		return {(float)x, (float)y};
+	}
 };
 
 struct Vec3 {
 	float x;
 	float y;
 	float z;
+
+	static Vec3 zero() {
+		return {0.0, 0.0, 0.0};
+	}
+
+	static Vec3 create(float x, float y, float z) {
+		return {x, y, z};
+	}
+
+	static Vec3 create(int x, int y, int z) {
+		return {(float)x, (float)y, (float)z};
+	}
 };
 
 struct Quad {
